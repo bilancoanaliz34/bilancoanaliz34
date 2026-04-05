3189a8a962bb9e2a3199ea1a6322bbdd553398d7
bilancoanaliz34 <sametcan012@gmail.com>
2026-04-05 13:50:26 +0300

    Update generate_hisse.py

--- generate_hisse.py
+++ generate_hisse.py
@@ -448,7 +448,7 @@ async function xPaylas(){
 <body>
 <script data-cfasync="false">
 var VERI = {hisse_json};
-var LOGOS = {{}};
+if(!window.LOGOS) window.LOGOS = {{}};  // logos.js zaten yüklediyse sıfırlama
 var D = {{}};
 var CHS = [];
 var activePeriod = '';
